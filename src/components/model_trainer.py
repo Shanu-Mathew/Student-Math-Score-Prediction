c93fe523c2cde7fc625110786b64c60bdb845326
Shanu Mathew <shanutmathew1703@gmail.com>
2023-04-22 23:52:24 +0530

    Adding Music to the website

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -80,7 +80,7 @@ class ModelTrainer:
             best_model=models[best_model_name]
 
             if best_model_score<0.6:
-                raise CustomException("No best model foound")
+                raise CustomException("No best model found")
             logging.info("Best found model on both training and testing dataset")
 
             save_object(
